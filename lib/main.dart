c4400ce0329c7bae7a3601561be3fdb09a7bd1ba
sobananjum <soban.developlogix@gmail.com>
2022-09-01 17:45:25 +0500

    - PDF Viewer

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:player_exchange/app.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp( PlayerExchange());
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const PlayerExchange());
+
 }
 
 
